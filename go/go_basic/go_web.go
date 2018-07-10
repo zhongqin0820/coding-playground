2a4facde628205b3d5047a7664da53b2f7644b4b
zhongqin0820 <zhongqin0820@163.com>
2018-07-10 20:56:51 +0800

    go http/template

--- go/go_basic/go_web.go
+++ go/go_basic/go_web.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"html/template"
 	"log"
 	"net/http"
 	"strings"
@@ -16,8 +17,22 @@ func sayhelloName(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Hello...")
 }
 
+func login(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("method: ", r.Method)
+	if r.Method == "GET" {
+		t, _ := template.ParseFiles("login.gtpl")
+		log.Println(t.Execute(w, nil))
+	} else {
+		// 需要显式调用request.ParseForm()解析表单数据
+		r.ParseForm()
+		fmt.Println("username:", r.Form["username"])
+		fmt.Println("password:", r.Form["password"])
+	}
+}
+
 func main() {
 	http.HandleFunc("/", sayhelloName)
+	http.HandleFunc("/login", login)
 	err := http.ListenAndServe("localhost:9999", nil)
 	if err != nil {
 		log.Fatal("ListenAndServe:", err)

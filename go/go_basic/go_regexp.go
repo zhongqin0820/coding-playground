186048b30765b05c603520948a1a7e007bdba561
zhongqin0820 <zhongqin0820@163.com>
2018-07-11 14:13:13 +0800

    go regexp package

---
+++ go/go_basic/go_regexp.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"regexp"
+	"strings"
+)
+
+func main() {
+	resp, err := http.Get("http://www.baidu.com")
+	if err != nil {
+		fmt.Println("http get error.")
+	}
+	defer resp.Body.Close()
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		fmt.Println("http read err")
+		return
+	}
+	src := string(body)
+	// 标签转换成小写
+	re, _ := regexp.Compile("\\<[\\S\\s]+?\\>")
+	src = re.ReplaceAllStringFunc(src, strings.ToLower)
+
+	// delete style
+	re, _ = regexp.Compile("\\<style[\\S\\s]+?\\>")
+	src = re.ReplaceAllString(src, "")
+
+	// delete script
+	re, _ = regexp.Compile("\\<script[\\S\\s]+?\\</script\\>")
+	src = re.ReplaceAllString(src, "")
+
+	// delete <
+	re, _ = regexp.Compile("\\<[\\S\\s]+?\\>")
+	src = re.ReplaceAllString(src, "\n")
+
+	// delete n
+	re, _ = regexp.Compile("\\s{2,}")
+	src = re.ReplaceAllString(src, "\n")
+
+	fmt.Println(strings.TrimSpace(src))
+}

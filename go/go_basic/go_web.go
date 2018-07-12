20cac94b32aeb3358a089b85c273f6b6ee50104a
zhongqin0820 <zhongqin0820@163.com>
2018-07-12 10:24:24 +0800

    go csrf

--- go/go_basic/go_web.go
+++ go/go_basic/go_web.go
@@ -1,11 +1,15 @@
 package main
 
 import (
+	"crypto/md5"
 	"fmt"
 	"html/template"
+	"io"
 	"log"
 	"net/http"
+	"strconv"
 	"strings"
+	"time"
 )
 
 func sayhelloName(w http.ResponseWriter, r *http.Request) {
@@ -20,13 +24,24 @@ func sayhelloName(w http.ResponseWriter, r *http.Request) {
 func login(w http.ResponseWriter, r *http.Request) {
 	fmt.Println("method: ", r.Method)
 	if r.Method == "GET" {
+		// 生成随机token
+		h := md5.New()
+		crutime := time.Now().Unix()
+		io.WriteString(h, strconv.FormatInt(crutime, 10))
+		io.WriteString(h, "ajggeruag")
+		token := fmt.Sprintf("%x", h.Sum(nil))
 		t, _ := template.ParseFiles("login.gtpl")
-		log.Println(t.Execute(w, nil))
+		log.Println(t.Execute(w, token))
 	} else {
 		// 需要显式调用request.ParseForm()解析表单数据
 		r.ParseForm()
-		fmt.Println("username:", r.Form["username"])
-		fmt.Println("password:", r.Form["password"])
+		token := r.Form.Get("token")
+		if token != "" {
+			fmt.Println("username:", r.Form["username"])
+			fmt.Println("password:", r.Form["password"])
+		} else {
+			fmt.Println("token is nil")
+		}
 	}
 }
 
--- go/go_basic/login.gtpl
+++ go/go_basic/login.gtpl
@@ -4,6 +4,7 @@
 </head>
 <body>
 <form action="/login" method="post">
+	<input type="hidden" name="token" value={{.}}>
 	用户名:<input type="text" name="username">
 	密码:<input type="password" name="password">
 	<input type="submit" value="登录">

7866e3c8665342a53adbf44ad8d23baeb33f1d49
zhongqin0820 <zhongqin0820@163.com>
2018-07-19 15:38:44 +0800

    learning strings strconv time packages in go

--- go/go_intro/chap4.go
+++ go/go_intro/chap4.go
@@ -2,7 +2,10 @@ package main
 
 import (
 	"fmt"
+	"log"
+	"strconv"
 	"strings"
+	"time"
 )
 
 func exp1() {
@@ -26,11 +29,49 @@ func exp3() {
 }
 
 func exp4() {
-	s := "test about strings package suffix"
+	s := "test about strings package test suffix"
 	res := strings.HasPrefix(s, "test")
 	fmt.Println("HasPrefix: ", res)
 	res = strings.HasSuffix(s, "suffix")
 	fmt.Println("HasSuffix: ", res)
+	res = strings.Contains(s, "test")
+	log.Println("Contains", res)
+	resInt := strings.Index(s, "test")
+	log.Println("Index", resInt)
+	// log.Fatalln("Index", resInt)
+	// log.Panicln("Index", resInt)
+	resInt = strings.LastIndex(s, "test")
+	log.Println("Index", resInt)
+	resString := strings.Replace(s, "es", "hhhhhhhhh", 1)
+	log.Println("Replace", resString)
+	resInt = strings.Count(s, "test")
+	log.Println("Count", resInt)
+	resString = strings.Repeat("test ", 3)
+	log.Println(resString)
+	resString = strings.ToUpper(resString)
+	log.Println(resString)
+	resString = strings.ToLower(resString)
+	log.Println(resString)
+	testString := "     resr   rserrrrrrrrrrrrrrr     "
+	testString = strings.TrimSpace(testString)
+	log.Println(testString)
+	testString = strings.Trim(testString, "r")
+	log.Println(testString)
+	resSlice := strings.Fields(s)
+	log.Printf("%v", resSlice)
+	log.Println(len(resSlice))
+	resSlice = strings.Split(s, " ")
+	log.Printf("%v", resSlice)
+	resString = strings.Join(resSlice, ",")
+	log.Println(resString)
+	convInt, err := strconv.Atoi("123")
+	if err != nil {
+		log.Printf("%T", err)
+	} else {
+		log.Println(convInt, err)
+	}
+	t := time.Now()
+	log.Println(t, "\n", t.Day(), t.Month(), t.Hour())
 	return
 }
 

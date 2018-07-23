f5f62a5785cc9a36a1fe073923e971bd6164d0ef
zhongqin0820 <zhongqin0820@163.com>
2018-07-24 00:07:34 +0800

    print pascal array

---
+++ go/goLeetCode/pascalArray.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"log"
+)
+
+func generate(numRows int) [][]int {
+	if numRows <= 0 {
+		return nil
+	}
+	var res [][]int
+	for i := 0; i < numRows; i++ {
+		var subRes []int = make([]int, i+1, i+1)
+		for j := 0; j <= i; j++ {
+			log.Println(i, j)
+			if j == 0 || j == i {
+				subRes[j] = 1
+			} else {
+				subRes[j] = res[i-1][j-1] + res[i-1][j]
+			}
+		}
+		res = append(res, subRes)
+		log.Println(res)
+	}
+	return res
+}
+
+func main() {
+	Res := generate(5)
+	log.Println(Res)
+}

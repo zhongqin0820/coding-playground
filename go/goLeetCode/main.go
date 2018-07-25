6c25e27a0e99fe8cd927ec101607afaf21aade5a
zhongqin0820 <zhongqin0820@163.com>
2018-07-25 22:58:27 +0800

    add reverse string problem solution

--- go/goLeetCode/main.go
+++ go/goLeetCode/main.go
@@ -2,12 +2,21 @@ package main
 
 import (
 	// c "./contestSet"
-	p "./problemSet"
+	// p "./problemSet"
 	"log"
 )
 
+func reverseString(s string) string {
+	var b []byte = []byte(s)
+	for i, j := 0, len(b)-1; i < j; i, j = i+1, j-1 {
+		b[j], b[i] = b[i], b[j]
+	}
+	return string(b)
+}
+
 func main() {
 	// res := p.AddBinary("110010", "10111")
-	res := p.StrStr("hello", "ll")
+	// res := p.StrStr("hello", "ll")
+	res := reverseString("hello")
 	log.Printf("%d\n", res)
 }

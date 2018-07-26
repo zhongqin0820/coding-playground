9ebf333602c0c582007f5273ddb3702023b15b6f
zhongqin0820 <zhongqin0820@163.com>
2018-07-26 13:55:33 +0800

    using two pointer to removeElements

--- go/goLeetCode/main.go
+++ go/goLeetCode/main.go
@@ -42,13 +42,29 @@ func twoSum(numbers []int, target int) []int {
 	return []int{left + 1, right + 1}
 }
 
+func removeElement(nums []int, val int) int {
+	j := 0
+	for _, v := range nums {
+		if v != val {
+			nums[j] = v
+			j++
+		}
+	}
+	nums = nums[:j]
+	log.Println(nums)
+	return len(nums)
+}
+
 func main() {
 	// res := p.AddBinary("110010", "10111")
 	// res := p.StrStr("hello", "ll")
 	// res := reverseString("hello")
 	// res := twoSum([]int{2, 7, 11, 15}, 9)
 	// res := twoSum([]int{2, 3, 4}, 6)
-	res := twoSum([]int{2, 3, 4, 5, 11, 17, 19}, 16)
+	// res := twoSum([]int{2, 3, 4, 5, 11, 17, 19}, 16)
 	// res := twoSum([]int{5, 25, 75}, 100)
+	// res := removeElement([]int{3, 2, 2, 3, 5, 6, 2, 5}, 2)
+	res := removeElement([]int{3, 2, 2, 3, 5, 6, 2, 5}, 1)
+	// res := removeElement([]int{}, 2)
 	log.Printf("%d\n", res)
 }

cdc555a1c7e56e67dd1924c4e9e7b85923d3b94b
zhongqin0820 <zhongqin0820@163.com>
2019-03-08 15:51:41 +0800

    go review basic sort algorithm

---
+++ go/goLeetCode/sortAlgorithm/simpleSort.go
@@ -0,0 +1,80 @@
+package sortAlgorithm
+
+import (
+	"log"
+)
+
+// i indicates the sort iteration, j indicates the swap operation
+func BubleSort(s []int, flag bool) []int {
+	for i, _ := range s {
+		for j := 0; j < len(s)-i-1; j++ {
+			if flag && s[j] < s[j+1] {
+				s[j], s[j+1] = s[j+1], s[j]
+			} else if !flag && s[j] > s[j+1] {
+				s[j], s[j+1] = s[j+1], s[j]
+			}
+		}
+		log.Println(s)
+	}
+	log.Println("Sort Over!")
+	return s
+}
+
+// iSorted indicates the last sorted index
+// introduce a temp to store the cache value to insert or use [iSorted+1]
+// in the sorted part, use binarySearch to find the postion is quicker than iterate to find
+func InsertSort(s []int, flag bool) []int {
+	var temp int
+	for iSorted := 0; iSorted < len(s)-1; iSorted++ {
+		if !flag && s[iSorted] > s[iSorted+1] {
+			temp = s[iSorted+1]
+			for j := iSorted; j >= 0; j-- {
+				if s[j] > temp {
+					s[j+1] = s[j]
+					s[j] = temp
+				} else {
+					s[j+1] = temp
+					break
+				}
+			}
+		} else if flag && s[iSorted] < s[iSorted+1] {
+			temp = s[iSorted+1]
+			for j := iSorted; j >= 0; j-- {
+				if s[j] < temp {
+					s[j+1] = s[j]
+					s[j] = temp
+				} else {
+					s[j+1] = temp
+				}
+			}
+		}
+		log.Println(s)
+	}
+	return s
+}
+
+// use temp to instore the temp position of the min/max value of the unsorted part
+// always select the min/max value from the unsorted part
+func SelectSort(s []int, flag bool) []int {
+	var temp int
+	for i := 0; i < len(s)-1; i++ {
+		temp = i
+		if flag {
+			for j := i + 1; j < len(s); j++ {
+				if s[j] < s[temp] {
+					temp = j
+				}
+			}
+			s[i], s[temp] = s[temp], s[i]
+		} else {
+			for j := i + 1; j < len(s); j++ {
+				if s[j] > s[temp] {
+					temp = j
+				}
+			}
+			s[i], s[temp] = s[temp], s[i]
+		}
+		log.Println(s)
+	}
+	return s
+}
---
+++ go/goLeetCode/sortAlgorithm/sort_test.go
@@ -0,0 +1,50 @@
+package sortAlgorithm
+
+import (
+	"testing"
+)
+
+// go test -v -test.run TestBubleSort
+func TestBubleSort(t *testing.T) {
+	s := []int{3, 6, 4, 2, 11, 10, 5}
+	s1 := BubleSort(s, false) //small -> big
+	t.Log(s1)
+	s2 := BubleSort(s, true) //big -> small
+	t.Log(s2)
+}
+
+// go test -v -test.run TestInsertSort
+func TestInsertSort(t *testing.T) {
+	s := []int{3, 6, 4, 2, 11, 10, 5}
+	s1 := InsertSort(s, false) //small -> big
+	t.Log(s1)
+	s2 := InsertSort(s, true) //big -> small
+	t.Log(s2)
+}
+
+// go test -v -test.run SelectSort
+func TestSelectSort(t *testing.T) {
+	s := []int{3, 1, 4, 2, 11, 10, 5}
+	s1 := InsertSort(s, false) //small -> big
+	t.Log(s1)
+	s2 := InsertSort(s, true) //big -> small
+	t.Log(s2)
+}
+
+// go test -v -test.run MergeSort
+func TestMergeSort(t *testing.T) {
+	s := []int{3, 1, 4, 2, 11, 10, 5}
+	s1 := MergeSort(s, false) //small -> big
+	t.Log(s1)
+	s2 := MergeSort(s, true) //big -> small
+	t.Log(s2)
+}
+
+// go test -v -test.run QuickSort
+func TestQuickSort(t *testing.T) {
+	s := []int{3, 1, 4, 2, 11, 10, 5}
+	s1 := QuickSort(s, false) //small -> big
+	t.Log(s1)
+	s2 := QuickSort(s, true) //big -> small
+	t.Log(s2)
+}

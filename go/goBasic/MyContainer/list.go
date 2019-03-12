825934f5b2acdbd6f84984743ed3100aadc02120
zhongqin0820 <zhongqin0820@163.com>
2019-03-12 21:51:22 +0800

    backup go learning code

---
+++ go/goBasic/MyContainer/list.go
@@ -0,0 +1,39 @@
+package MyContainer
+
+import (
+	"container/list"
+	"fmt"
+)
+
+func IteList() {
+	var l = list.New()
+	l.PushBack(4)
+	e2 := l.PushFront(2)
+	l.InsertAfter(3, e2)
+	l.InsertBefore(1, e2)
+	for e := l.Front(); e != nil; e = e.Next() {
+		fmt.Print(e.Value)
+	}
+}
+
+// struct is not support to compare
+type Person struct {
+	Name string
+	Age  int
+}
+
+func StructFea() {
+	p1 := &Person{Name: "A", Age: 12}
+	p2 := &Person{Name: "B", Age: 12}
+	p3 := &Person{Name: "A", Age: 12}
+	if p1 == p2 {
+		fmt.Println("Equal")
+	} else {
+		fmt.Println("Not Equal")
+	}
+	if p1 == p3 {
+		fmt.Println("Equal")
+	} else {
+		fmt.Println("Not Equal")
+	}
+}
---
+++ go/goBasic/MyContainer/list_test.go
@@ -0,0 +1,14 @@
+package MyContainer
+
+import (
+	"testing"
+)
+
+func TestIteList(t *testing.T) {
+	IteList()
+}
+
+// go test -v -test.run TestStructFea
+func TestStructFea(t *testing.T) {
+	StructFea()
+}

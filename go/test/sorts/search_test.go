3cec030f6e68f46ead2bbe35fb85c123af1e0a1d
zhongqin0820 <zhongqin0820@163.com>
2019-06-05 22:26:44 +0800

    add interpolation search implementation and simple unit test case

--- go/test/sorts/search_test.go
+++ go/test/sorts/search_test.go
@@ -4,22 +4,44 @@ import (
 	"testing"
 )
 
-// go test -v -test.run TestBinarySearch
-func TestBinarySearch(t *testing.T) {
+// go test -v -test.run TestSearch
+func TestSearch(t *testing.T) {
 	a := []int{3, 5, 1, 6, 7, 9, 10}
 	a = QuickSort(a, false)
-	// t.Log(a)
-	target := 5
-	if x := BinarySearch(a, target); x == -1 {
-		t.Error("Error")
-	} else {
-		t.Log(x)
-	}
-	target = -4
-	if x := BinarySearch(a, target); x != -1 {
-		t.Error("Error")
-	} else {
-		t.Log(target)
-	}
 
+	t.Run("BinarySearch 1", func(t *testing.T) {
+		target := 5
+		if x := BinarySearch(a, target); x == -1 {
+			t.Error("Error")
+		} else {
+			t.Log(x)
+		}
+	})
+
+	t.Run("BinarySearch 2", func(t *testing.T) {
+		target := -4
+		if x := BinarySearch(a, target); x != -1 {
+			t.Error("Error")
+		} else {
+			t.Log(target)
+		}
+	})
+
+	t.Run("InterpolationSearch 1", func(t *testing.T) {
+		target := 5
+		if x := InterpolationSearch(a, target); x == -1 {
+			t.Error("Error")
+		} else {
+			t.Log(target)
+		}
+	})
+
+	t.Run("InterpolationSearch 2", func(t *testing.T) {
+		target := -4
+		if x := InterpolationSearch(a, target); x != -1 {
+			t.Error("Error")
+		} else {
+			t.Log(target)
+		}
+	})
 }
--- go/test/sorts/simpleSearch.go
+++ go/test/sorts/simpleSearch.go
@@ -15,3 +15,21 @@ func BinarySearch(s []int, target int) int {
 	}
 	return -1
 }
+
+func InterpolationSearch(a []int, target int) int {
+	i, mid, j, n := 0, 0, len(a)-1, len(a)-1
+	for i <= j {
+		mid = i + (j-i)*(target-a[i])/(a[j]-a[i])
+		if mid < 0 || mid > n {
+			break
+		}
+		if a[mid] == target {
+			return mid
+		} else if a[mid] < target {
+			i = mid + 1
+		} else {
+			j = mid - 1
+		}
+	}
+	return -1
+}

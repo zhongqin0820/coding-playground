1a43d08b1b1dc31acf0db133f69b0ff0ddba6435
zhongqin0820 <zhongqin0820@163.com>
2019-06-10 22:59:43 +0800

    sort out files

---
+++ go/concurrent/snippet/pool_test.go
@@ -0,0 +1,38 @@
+package snippet
+
+import (
+	"bytes"
+	"io"
+	"os"
+	"sync"
+	"testing"
+	"time"
+)
+
+// demo code from https://golang.org/pkg/sync/#Pool
+var bufPool = sync.Pool{
+	New: func() interface{} {
+		return new(bytes.Buffer)
+	},
+}
+
+func timeNow() time.Time {
+	return time.Unix(1136214245, 0)
+}
+
+func Log(w io.Writer, key, val string) {
+	b := bufPool.Get().(*bytes.Buffer)
+	b.Reset()
+	//
+	b.WriteString(timeNow().UTC().Format(time.RFC3339))
+	b.WriteByte(' ')
+	b.WriteString(key)
+	b.WriteByte('=')
+	b.WriteString(val)
+	w.Write(b.Bytes())
+	bufPool.Put(b)
+}
+
+func TestLog(t *testing.T) {
+	Log(os.Stdout, "path", "/search?q=flowers")
+}
---
+++ go/concurrent/snippet/worker_test.go
@@ -0,0 +1,34 @@
+package snippet
+
+import (
+	"log"
+	"testing"
+	"time"
+	"unsafe"
+)
+
+func worker(id int, jobs <-chan int, results chan<- int) {
+	for j := range jobs {
+		log.Println("worker", id, "started job", j)
+		time.Sleep(1e9)
+		log.Println("worker", id, "finished job", j)
+		results <- j * 2
+	}
+}
+
+func TestWorker(t *testing.T) {
+	jobs := make(chan int, 100)
+	results := make(chan int, 100)
+	for w := 1; w < 4; w++ {
+		go worker(w, jobs, results)
+	}
+	for j := 1; j < 6; j++ {
+		jobs <- j
+	}
+	close(jobs)
+	t.Log(unsafe.Sizeof(results), len(results), cap(results))
+	for a := 1; a < 6; a++ {
+		t.Log(a, <-results)
+	}
+	t.Log(unsafe.Sizeof(results), len(results), cap(results))
+}

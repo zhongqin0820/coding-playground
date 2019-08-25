1c4d6d17b2203cccf17cc5bd511621f00a3fbbee
zhongqin0820 <zhongqin0820@163.com>
2019-08-25 18:46:33 +0800

    add gpu-top & sort out

--- python/README.md
+++ python/README.md
@@ -7,26 +7,7 @@ This folder contains the practice codes of python scripts.
 # Structure
 ```sh
 .
-├── README.md
-├── basic           #the practices of python: data structures etc.
-├── game_predict    #house the code of game predict contest
-├── leetCode        #house the code of LeetCode
-├── offer           #house the code of CodingInterviews
-├── python3DAML     #house the code of Python 3: Data Analysis and Machine Learning
-├── test.py         #simple demo file
-├── words_crawler   #words downloader using youdao API
-└── xgboost         #the practices of xgboost machine learning framework
+├── basics          #basic snippets
+├── miscs           #misc useful tool helper in python
+└── tutorials       #tutorials code
 ```
-
-# Ref
-- [game predict contest](http://www.dcjingsai.com/common/cmpt/%E6%B8%B8%E6%88%8F%E7%8E%A9%E5%AE%B6%E4%BB%98%E8%B4%B9%E9%87%91%E9%A2%9D%E9%A2%84%E6%B5%8B%E5%A4%A7%E8%B5%9B_%E8%B5%9B%E4%BD%93%E4%B8%8E%E6%95%B0%E6%8D%AE.html)
-- [LeetCode](https://leetcode.com/)
-- [_CodingInterviews_](https://book.douban.com/subject/6966465/)
-- [_Python 3: Data Analysis and Machine Learning_](https://book.douban.com/subject/30310955/)
-- [youdao.com](http://www.youdao.com/)
-
-## Publications
-- [ ] [Python进阶](https://github.com/eastlakeside/interpy-zh/blob/master/SUMMARY.md)
-- [x] [Python 3 教程](http://www.runoob.com/python3/python3-tutorial.html)
-- [x] [廖雪峰Python3教程](https://www.liaoxuefeng.com/wiki/0014316089557264a6b348958f449949df42a6d3a2e542c000)
-- [x] [Python 基础教程](http://www.runoob.com/python/python-tutorial.html)
---
+++ python/miscs/gpu_top.py
@@ -0,0 +1,68 @@
+import subprocess
+import re
+
+gpu_stats = subprocess.getoutput("nvidia-smi").split('\n')
+n_line = len(gpu_stats)
+
+print(gpu_stats[0])
+print("+------------------------------------------------------------------------------+")
+print("| GPU    Fan  Temp  Perf  Pwr:Usage/Cap             Memory-Usage   Utilization |")
+print("|==============================================================================|")
+
+# find gpu info start
+i = 1
+while i < n_line and not gpu_stats[i].startswith("|=="):
+    i += 1
+i += 1
+
+# print gpu info
+while i < n_line:
+    if gpu_stats[i].startswith("+--"):
+        i += 1
+    elif gpu_stats[i].startswith("|"):
+        gpu_id = int(gpu_stats[i][1:5])
+        gpu_info = gpu_stats[i+1].split('|')
+        gpu_temp = gpu_info[1].strip()
+        gpu_mem = gpu_info[2].strip()
+        gpu_utl = gpu_info[3][:8]
+        print("| {} {} {} {} |".format(str(gpu_id).rjust(3), gpu_temp.rjust(32), gpu_mem.rjust(25), gpu_utl.rjust(13)))
+        i += 2
+    else:
+        break
+
+# find pid info start
+while i < n_line and not gpu_stats[i].startswith("|=="):
+    i += 1
+i += 1
+
+# print pid info
+pid_results = []
+pids = []
+while i < n_line:
+    if gpu_stats[i].startswith("+--") or gpu_stats[i].startswith("|  No"):
+        i += 1
+    elif gpu_stats[i].startswith("|"):
+        pid_info = re.split(r'\s+', gpu_stats[i])
+        gpu_id = pid_info[1]
+        pid = pid_info[2]
+        pmem = pid_info[-2]
+        pid_results.append((gpu_id, pid, pmem))
+        pids.append(pid)
+        i += 1
+    else:
+        break
+
+print("+------------------------------------------------------------------------------+")
+print("| GPU    PID  User           Process name                               Memory |")
+print("|==============================================================================|")
+
+if len(pids) > 0:
+    pid_stats = subprocess.getoutput("ps -o pid,user:14,cmd -p {}".format(",".join(pids))).split('\n')
+    pid_pair = [re.split(r'\s+', line.strip(), 2) for line in pid_stats[1:]]
+    pid2user = {pair[0]: pair[1] for pair in pid_pair}
+    pid2cmd = {pair[0]: pair[2] for pair in pid_pair}
+    for gpu_id, pid, pmem in pid_results:
+        print("| {} {}  {} {} {} |".format(gpu_id.rjust(3), pid.rjust(6), pid2user[pid][:14].ljust(14), pid2cmd[pid][:39].ljust(39), pmem.rjust(9)))
+else:
+    print("|  No running processes found                                                  |")
+print("+------------------------------------------------------------------------------+")

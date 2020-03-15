1208c1a8b00633fbe67f9ada3a71a2d97806061f
zhongqin0820 <zhongqin0820@163.com>
2020-03-15 18:14:51 +0800

    Fix make issue

--- others/tutorials/unix/assignment/README.md
+++ others/tutorials/unix/assignment/README.md
@@ -1,5 +1,5 @@
 # [The Unix Workbench](https://seankross.com/the-unix-workbench) course assignment
 ## Date
-2020-03-08 14:50:31
+2020-03-09 22:17:00
 ## Lines in guessinggame.sh
-21
+25
--- others/tutorials/unix/assignment/guessinggame.sh
+++ others/tutorials/unix/assignment/guessinggame.sh
@@ -1,11 +1,15 @@
 #!/usr/bin/env bash
 function ask {
     echo "Hey, guess how many files are in the current directory..."
-    read guess
+    read -p "Please input an integer: " guess
+    if [[ ! $guess =~ ^[0-9]+$ ]]; then
+        echo "Your input is illegal!"
+        ask
+    fi
 }
 
 function main {
-    files=$(ls -1 | wc -l)
+    files=$(ls | wc -l)
     ask
     while [[ $guess -ne $files ]]; do
         if [[ $guess -lt $files ]]; then

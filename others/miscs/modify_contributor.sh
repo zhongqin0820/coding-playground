a370d302ed93aca8214f0f2774a76e216b443d63
zhongqin0820 <zhongqin0820@163.com>
2020-04-19 22:40:05 +0800

    Add git contributor modifying scripts

---
+++ others/miscs/modify_contributor.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+# Modify git log submitter info
+
+git filter-branch --env-filter '
+
+OLD_EMAIL="name@example.com"
+CORRECT_NAME="name"
+CORRECT_EMAIL="name@example.com"
+
+if [ "$GIT_COMMITTER_EMAIL" = "$OLD_EMAIL" ]
+then
+    export GIT_COMMITTER_NAME="$CORRECT_NAME"
+    export GIT_COMMITTER_EMAIL="$CORRECT_EMAIL"
+fi
+if [ "$GIT_AUTHOR_EMAIL" = "$OLD_EMAIL" ]
+then
+    export GIT_AUTHOR_NAME="$CORRECT_NAME"
+    export GIT_AUTHOR_EMAIL="$CORRECT_EMAIL"
+fi
+' --tag-name-filter cat -- --branches --tags
